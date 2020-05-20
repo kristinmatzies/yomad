7dcda1fbe921b465c6d123c728cf95b694e137bc
Kristin Matzies <kristinmatzies@hotmail.de>
2020-05-20 17:11:12 +0200

    Change cypress tests

--- cypress/integration/saveEvent.spec.js
+++ cypress/integration/saveEvent.spec.js
@@ -9,6 +9,10 @@ describe('last added event should automatically be on page /saved', () => {
 
 describe('last added event should not be on /saved page when false', () => {
   it('deletes the newly added event from saved when clicked', () => {
+    cy.visit('/')
+    cy.get(
+      ':nth-child(1) > [data-cy=eventtext] > [data-cy=save_button]'
+    ).click()
     cy.visit('/saved')
     cy.get(
       ':nth-child(1) > [data-cy=eventtext] > [data-cy=save_button]'
--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import Header from './Header'
+import { MemoryRouter } from 'react-router-dom'
 
 test('renders header with logo', () => {
-  const { getByAltText } = render(<Header />)
+  const { getByAltText } = render(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>
+  )
   const linkElement = getByAltText(/yomad/i)
   expect(linkElement).toBeInTheDocument()
 })
