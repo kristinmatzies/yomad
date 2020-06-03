58f6b717b43cde116aa274e0dcf3a1e2eba50eae
Kristin Matzies <kristinmatzies@hotmail.de>
2020-06-03 16:09:30 +0200

    Adjust cypress tests to work correctly

--- cypress/integration/googleMaps.spec.js
+++ cypress/integration/googleMaps.spec.js
@@ -5,7 +5,7 @@ describe('navigates to Google Maps', () => {
       ':nth-child(1) > [data-cy=eventtext] > :nth-child(4) > [data-cy=google_link]'
     ).then(function ($a) {
       const href = $a.prop('href')
-      cy.request(href).its('body').should('include', 'content="Google Maps"')
+      cy.request(href).its('body').should('include', 'Google Maps')
     })
   })
 })
